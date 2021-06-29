0c9235cf8edcee17c2a3133501b0376937d8acc3
José Carlos Palma <palmahn@gmail.com>
2021-06-29 01:21:07 -0600

    Páginas principales de la aplicación.

---
+++ src/components/dc/DCPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+export const DCPage = () => {
+    return (
+        <>
+            <Typography variant="h1">DC</Typography>
+        </>
+    );
+};
---
+++ src/components/marvel/MarvelPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+export const MarvelPage = () => {
+    return (
+        <>
+            <Typography variant="h1">Marvel</Typography>
+        </>
+    );
+};
