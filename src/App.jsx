e51a43d8d4ce9a758baac98f69a1455265295b1f
José Carlos Palma <palmahn@gmail.com>
2021-06-28 22:38:17 -0600

    Preparación y depuración inicial de la aplicación.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+// import { styled } from '@material-ui/core/styles';
+// import { spacing } from '@material-ui/system';
+// const Button = styled(ButtonX)(spacing);
+
+export const App = ({ title }) => {
+    return (
+        <>
+            <Typography variant="h6">Heros App</Typography>
+        </>
+    );
+};
